20dde31f14d353c9bbbd5d6ae226f6ca4b868d67
X0KA <36265613+X0KA@users.noreply.github.com>
2018-04-19 21:44:54 +0200

    Revert "Revert "Ball module implemented""
    This reverts commit 93ffee460f237fce8e8358bcb89484cd7ccecdce, reversing
    changes made to 074bdd5d7c4b5713804df6de6ccc9f43e42c7ed2.

--- Last Resort/Application.h
+++ Last Resort/Application.h
@@ -3,7 +3,7 @@
 
 #include "Globals.h"
 
-#define NUM_MODULES 17
+#define NUM_MODULES 18
 
 class ModuleWindow;
 class ModuleInput;
@@ -23,6 +23,7 @@ class ModuleCollision;
 class ModuleEnemies;
 class ModuleFirstBoss;
 class Module;
+class ModuleBall;
 
 
 class Application
@@ -47,6 +48,7 @@ public:
 	ModuleAudio* audio;
 	ModuleEnemies* enemies;
 	ModuleFirstBoss* boss1;
+	ModuleBall* ball_player1;
 
 public:
 
