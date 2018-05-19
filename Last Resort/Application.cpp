3395e96386430cc07f16f6dd543a24304ffdf743
urisabate <oriolsabate97@hotmail.com>
2018-05-19 13:32:15 +0200

    testing github 2

--- Last Resort/Application.cpp
+++ Last Resort/Application.cpp
@@ -38,6 +38,8 @@ Application::Application()
 	modules[i++] = ball_player2 = new ModuleBall2();
 	modules[i++] = player = new ModulePlayer();
 	modules[i++] = player2 = new ModulePlayer2();
+	modules[i++] = particles = new ModuleParticles();
+	
 	modules[i++] = scene_intro = new ModuleSceneIntro();
 	modules[i++] = fade = new ModuleFadeToBlack();
 }	
